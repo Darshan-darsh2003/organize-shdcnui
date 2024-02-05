bfca8eb3b3b9f1692452193b2123a43b738e617d
Darshan-darsh2003 <poojaridarsu9550@gmail.com>
2024-02-05 19:45:20 +0530

    new commit

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,69 +1,106 @@
+"use client"
+
 import React from "react"
+
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 const Page = () => {
   return (
-    <div className="flex gap-6 flex-col">
-      <div>
-        Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Sunt aut cumque facilis, iure ullam laudantium nesciunt reprehenderit
-        voluptatibus nobis ducimus exercitationem tempore, corrupti libero in
-        nemo eveniet quos neque maxime doloribus similique quia. Similique ea
-        hic expedita, voluptates delectus, aspernatur corrupti ullam sed
-        laboriosam ipsa animi suscipit nostrum quos obcaecati!
-      </div>
-      <div>
-        Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Sunt aut cumque facilis, iure ullam laudantium nesciunt reprehenderit
-        voluptatibus nobis ducimus exercitationem tempore, corrupti libero in
-        nemo eveniet quos neque maxime doloribus similique quia. Similique ea
-        hic expedita, voluptates delectus, aspernatur corrupti ullam sed
-        laboriosam ipsa animi suscipit nostrum quos obcaecati!
-      </div>
+    <>
+      <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
+        Your Notes
+      </h2>
+      <ScrollArea className="h-[80vh] w-[55vw] rounded-md border p-4">
+        <div className="flex gap-6 flex-col">
+          <div>
+            Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sunt aut cumque facilis, iure ullam laudantium nesciunt
+            reprehenderit voluptatibus nobis ducimus exercitationem tempore,
+            corrupti libero in nemo eveniet quos neque maxime doloribus
+            similique quia. Similique ea hic expedita, voluptates delectus,
+            aspernatur corrupti ullam sed laboriosam ipsa animi suscipit nostrum
+            quos obcaecati!
+          </div>
+          <div>
+            Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sunt aut cumque facilis, iure ullam laudantium nesciunt
+            reprehenderit voluptatibus nobis ducimus exercitationem tempore,
+            corrupti libero in nemo eveniet quos neque maxime doloribus
+            similique quia. Similique ea hic expedita, voluptates delectus,
+            aspernatur corrupti ullam sed laboriosam ipsa animi suscipit nostrum
+            quos obcaecati!
+          </div>
 
-      <div>
-        Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Sunt aut cumque facilis, iure ullam laudantium nesciunt reprehenderit
-        voluptatibus nobis ducimus exercitationem tempore, corrupti libero in
-        nemo eveniet quos neque maxime doloribus similique quia. Similique ea
-        hic expedita, voluptates delectus, aspernatur corrupti ullam sed
-        laboriosam ipsa animi suscipit nostrum quos obcaecati!
-      </div>
+          <div>
+            Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sunt aut cumque facilis, iure ullam laudantium nesciunt
+            reprehenderit voluptatibus nobis ducimus exercitationem tempore,
+            corrupti libero in nemo eveniet quos neque maxime doloribus
+            similique quia. Similique ea hic expedita, voluptates delectus,
+            aspernatur corrupti ullam sed laboriosam ipsa animi suscipit nostrum
+            quos obcaecati!
+          </div>
 
-      <div>
-        Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Sunt aut cumque facilis, iure ullam laudantium nesciunt reprehenderit
-        voluptatibus nobis ducimus exercitationem tempore, corrupti libero in
-        nemo eveniet quos neque maxime doloribus similique quia. Similique ea
-        hic expedita, voluptates delectus, aspernatur corrupti ullam sed
-        laboriosam ipsa animi suscipit nostrum quos obcaecati!
-      </div>
-      <div>
-        Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Sunt aut cumque facilis, iure ullam laudantium nesciunt reprehenderit
-        voluptatibus nobis ducimus exercitationem tempore, corrupti libero in
-        nemo eveniet quos neque maxime doloribus similique quia. Similique ea
-        hic expedita, voluptates delectus, aspernatur corrupti ullam sed
-        laboriosam ipsa animi suscipit nostrum quos obcaecati!
-      </div>
+          <div>
+            Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sunt aut cumque facilis, iure ullam laudantium nesciunt
+            reprehenderit voluptatibus nobis ducimus exercitationem tempore,
+            corrupti libero in nemo eveniet quos neque maxime doloribus
+            similique quia. Similique ea hic expedita, voluptates delectus,
+            aspernatur corrupti ullam sed laboriosam ipsa animi suscipit nostrum
+            quos obcaecati!
+          </div>
+          <div>
+            Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sunt aut cumque facilis, iure ullam laudantium nesciunt
+            reprehenderit voluptatibus nobis ducimus exercitationem tempore,
+            corrupti libero in nemo eveniet quos neque maxime doloribus
+            similique quia. Similique ea hic expedita, voluptates delectus,
+            aspernatur corrupti ullam sed laboriosam ipsa animi suscipit nostrum
+            quos obcaecati!
+          </div>
 
-      <div>
-        Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Sunt aut cumque facilis, iure ullam laudantium nesciunt reprehenderit
-        voluptatibus nobis ducimus exercitationem tempore, corrupti libero in
-        nemo eveniet quos neque maxime doloribus similique quia. Similique ea
-        hic expedita, voluptates delectus, aspernatur corrupti ullam sed
-        laboriosam ipsa animi suscipit nostrum quos obcaecati!
-      </div>
+          <div>
+            Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sunt aut cumque facilis, iure ullam laudantium nesciunt
+            reprehenderit voluptatibus nobis ducimus exercitationem tempore,
+            corrupti libero in nemo eveniet quos neque maxime doloribus
+            similique quia. Similique ea hic expedita, voluptates delectus,
+            aspernatur corrupti ullam sed laboriosam ipsa animi suscipit nostrum
+            quos obcaecati!
+          </div>
 
-      <div>
-        Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Sunt aut cumque facilis, iure ullam laudantium nesciunt reprehenderit
-        voluptatibus nobis ducimus exercitationem tempore, corrupti libero in
-        nemo eveniet quos neque maxime doloribus similique quia. Similique ea
-        hic expedita, voluptates delectus, aspernatur corrupti ullam sed
-        laboriosam ipsa animi suscipit nostrum quos obcaecati!
-      </div>
-    </div>
+          <div>
+            Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sunt aut cumque facilis, iure ullam laudantium nesciunt
+            reprehenderit voluptatibus nobis ducimus exercitationem tempore,
+            corrupti libero in nemo eveniet quos neque maxime doloribus
+            similique quia. Similique ea hic expedita, voluptates delectus,
+            aspernatur corrupti ullam sed laboriosam ipsa animi suscipit nostrum
+            quos obcaecati!
+          </div>
+          <div>
+            Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sunt aut cumque facilis, iure ullam laudantium nesciunt
+            reprehenderit voluptatibus nobis ducimus exercitationem tempore,
+            corrupti libero in nemo eveniet quos neque maxime doloribus
+            similique quia. Similique ea hic expedita, voluptates delectus,
+            aspernatur corrupti ullam sed laboriosam ipsa animi suscipit nostrum
+            quos obcaecati!
+          </div>
+
+          <div>
+            Notes Pagen Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sunt aut cumque facilis, iure ullam laudantium nesciunt
+            reprehenderit voluptatibus nobis ducimus exercitationem tempore,
+            corrupti libero in nemo eveniet quos neque maxime doloribus
+            similique quia. Similique ea hic expedita, voluptates delectus,
+            aspernatur corrupti ullam sed laboriosam ipsa animi suscipit nostrum
+            quos obcaecati!
+          </div>
+        </div>
+      </ScrollArea>
+    </>
   )
 }
 
